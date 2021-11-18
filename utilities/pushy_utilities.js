d80e56aeb33f70a46805067925b97f50a0744613
NicolasDeBoer <nicolasdeboer@gmail.com>
2021-11-18 12:24:37 -0800

    sendContactRequestToIndividual added to pushy_utilities

--- utilities/pushy_utilities.js
+++ utilities/pushy_utilities.js
@@ -3,7 +3,7 @@ const Pushy = require('pushy');
 // Plug in your Secret API Key 
 const pushyAPI = new Pushy(process.env.PUSHY_API_KEY);
 
-//use to send message to a specific client by the token
+//use to send a message to a specific client using the token
 function sendMessageToIndividual(token, message) {
 
     //build the message for Pushy to send
@@ -27,8 +27,30 @@ function sendMessageToIndividual(token, message) {
     })
 }
 
+//use to send a contact request to a specific client using the token
+function sendContactRequestToIndividual(token, message) {
+
+    //build the message for Pushy to send
+    let data = {
+        "type": "contact",
+        "username": message
+    };
+
+    // Send push notification via the Send Notifications API
+    // https://pushy.me/docs/api/send-notifications
+    pushyAPI.sendPushNotification(data, token, {}, function (err, id) {
+        // Log errors to console
+        if (err) {
+            return console.log('Fatal Error', err);
+        }
+
+        // Log success
+        console.log('Push sent successfully! (ID: ' + id + ')');
+    })
+}
+
 //add other "sendTypeToIndividual" functions here. Don't forget to export them
 
 module.exports = {
-    sendMessageToIndividual
+    sendMessageToIndividual, sendContactRequestToIndividual
 }
