eeaf0a483a3067ff0a0a070417e3d3f8a175f5a0
NicolasDeBoer <nicolasdeboer@gmail.com>
2021-12-04 20:28:04 -0800

    apidox typo fix

--- routes/changepw.js
+++ routes/changepw.js
@@ -15,7 +15,7 @@ const router = express.Router()
 
 
 /**
- * @api {post} /contacts Request to change password
+ * @api {post} /changepw Request to change password
  * @apiName PostChangePW
  * @apiGroup ChangePW
  * 
